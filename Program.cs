574823f2b85ac82484b25849b43eb058aefc256c
Taufan Augusta <taufan.augusta@protonmail.com>
2022-06-28 15:25:05 +0700

    setup Host

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+    .Build();
+
+var host = Host.CreateDefaultBuilder()
+    .ConfigureServices((context, services) => {
+
+    })
+    .ConfigureLogging((context, logging) => {
+        logging.ClearProviders();
+        logging.AddConfiguration(context.Configuration.GetSection("Logging"));
+        logging.AddConsole();
+    })
+    .Build();
